618e2aa9a7c561afb71fe9f74b1f9b203ef5df36
Vad-Dem <vadikdemidenko@i.ua>
2022-09-29 19:10:16 +0300

    add experiense

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,19 @@
 import * as flsFunction from "./function.js";
-
 document.addEventListener("DOMContentLoaded", function () {
   flsFunction.isWebp();
+  new Swiper('.experiense-swiper', {
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
 });
