3e4a46292ad18d300e47f0c611bc50ba38b5e7a3
Vad-Dem <vadikdemidenko@i.ua>
2022-09-29 22:20:45 +0300

    end desctop version

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,8 @@ import * as flsFunction from "./function.js";
 document.addEventListener("DOMContentLoaded", function () {
   flsFunction.isWebp();
   new Swiper('.experiense-swiper', {
-  
+    
+    slidesPerView: 1,
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -15,5 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
       prevEl: '.swiper-button-prev',
     },
   
+  });
+  new Swiper('.company-swiper', {
+    loop: true,
+    slidesPerView: 6.5,
+    spaceBetween: 30,
+    grabCursor: true,
   });
 });
