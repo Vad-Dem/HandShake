4a10ed71bb196fa2aa5defee5b7da4695790ba37
Vad-Dem <vadikdemidenko@i.ua>
2022-10-01 15:31:08 +0300

    add animate

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,10 @@
 import * as flsFunction from "./function.js";
+import WOW from "../../node_modules/wowjs/dist/wow.js";
 document.addEventListener("DOMContentLoaded", function () {
   flsFunction.isWebp();
+  new WOW.WOW({
+    mobile: false,
+  }).init();
   new Swiper('.experiense-swiper', {
     
     slidesPerView: 1,
